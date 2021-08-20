7d89ab5073d04d97824fca63fd950874797cf21e
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-08-20 14:33:42 -0300

    Splash implamentation

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import "./Splash.css";
 import { Redirect } from "react-router-dom";
+import RandCandles from "../../containers/apresentationImages/RandCandles";
 
-function AnimatedSplash(props) {
+function AnimatedSplash() {
   return (
     <div className="logo_wrapper">
-      <div className="loading">
-        <div class="ball"></div>
-        <div class="ball"></div>
-        <div class="ball"></div>
-        <div class="ball"></div>
-        <div class="ball"></div>
-        <div class="ball"></div>
-        <div class="ball"></div>
-      </div>
+        <div className="candle_container">
+        <RandCandles />
+        </div>
     </div>
   );
 }
 
 function Splash(props) {
   const [redirect, setRedirect] = useState(false);
-  setTimeout(() => setRedirect(true), 2000);
+  setTimeout(() => setRedirect(true), 3000);
 
   return redirect ? (
     <Redirect to="/home" />
   ) : (
-    <AnimatedSplash theme={props.theme} />
+    <AnimatedSplash  />
   );
 }
 
